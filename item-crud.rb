a44d3e0e5d0f0def4abf645f293badcad2599a74
Galuh Estya <galuhestya@gmail.com>
2018-07-03 17:37:33 +0700

    add get, create, and update skeleton

--- item-crud.rb
+++ item-crud.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+configure do
+  $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
+  Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") { |lib| 
+    require File.basename(lib, '.*') 
+  }
+end
 
 get '/' do
-  "Hello World!"
+	"Hello World!"
+end
+
+get '/item/:id' do |id|
+	Item.get(id)
+end
+
+post '/item' do
+	name = params["name"]
+	Item.create(name)
+end
+ 
+post '/item/:id' do |id|
+	Item.update(id, params['name'])
 end
