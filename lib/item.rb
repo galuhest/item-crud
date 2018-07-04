99e96a6561df9ca0e7c191cf669ebbbd40f42a44
Galuh Estya <galuhestya@gmail.com>
2018-07-04 10:37:17 +0700

    spacing jadi 2

--- lib/item.rb
+++ lib/item.rb
@@ -3,23 +3,23 @@ require 'item/crud'
 # yang dilakukan terhadap tabel item. Library
 # Crud digunakan sebagai wrapper database.
 module Item
-	# Mendapatkan nama item dari ID item tersebut
-	def self.get(id)
-		item = Crud.get(id)
-	end
+  # Mendapatkan nama item dari ID item tersebut
+  def self.get(id)
+    item = Crud.get(id)
+  end
 
-	# Memasukan barang baru kedalam database
-	def self.create(name)
-		item = Crud.create(name)
-	end
+  # Memasukan barang baru kedalam database
+  def self.create(name)
+    item = Crud.create(name)
+  end
 
-	# Mengganti nama item berdasarkan ID item
-	def self.update(id, name)
-		item = Crud.update(id, name)
-	end
+  # Mengganti nama item berdasarkan ID item
+  def self.update(id, name)
+    item = Crud.update(id, name)
+  end
 
-	# Menghapus sebuah item berdasarkan ID
-	def self.delete(id)
-		status = Crud.delete(id)
-	end
+  # Menghapus sebuah item berdasarkan ID
+  def self.delete(id)
+    status = Crud.delete(id)
+  end
 end
